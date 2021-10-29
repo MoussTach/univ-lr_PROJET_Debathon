8b9b91d8cdca2060c03b244583d2c13f5a63805e
Gaetan Brenckle <gaetan.brenckle@outlook.fr>
2021-10-29 03:05:10 +0200

    [View] Create the Debate thumbnail view

---
+++ src/main/java/fr/univlr/debathon/application/view/mainwindow/debate/DebateThumbnailView.java
@@ -0,0 +1,68 @@
+package fr.univlr.debathon.application.view.mainwindow.debate;
+
+import de.saxsys.mvvmfx.InjectViewModel;
+import fr.univlr.debathon.application.view.FxmlView_SceneCycle;
+import fr.univlr.debathon.application.view.mainwindow.HomePageView;
+import fr.univlr.debathon.application.viewmodel.mainwindow.HomePageViewModel;
+import fr.univlr.debathon.application.viewmodel.mainwindow.debate.DebateThumbnailViewModel;
+import fr.univlr.debathon.log.generate.CustomLogger;
+import javafx.application.Platform;
+import javafx.collections.WeakListChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DebateThumbnailView extends FxmlView_SceneCycle<DebateThumbnailViewModel> implements Initializable {
+
+    private static final CustomLogger LOGGER = CustomLogger.create(DebateThumbnailView.class.getName());
+
+    @FXML private Label lblTitle;
+    @FXML private FlowPane flowTag;
+    @FXML private Label lblNbPeople;
+
+    @FXML private Button btnOpenDebate;
+
+    @InjectViewModel
+    private DebateThumbnailViewModel debateThumbnailViewModel;
+
+
+    @FXML
+    public void act_btnOpenDebate() {
+        LOGGER.input(String.format("Press the button %s", btnOpenDebate.getId()));
+
+        System.out.println(String.format("Open debate [%s]", lblTitle.getText()));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setViewModel(debateThumbnailViewModel);
+
+        //Value
+        this.lblTitle.textProperty().bind(this.debateThumbnailViewModel.lblTitle_valueProperty());
+        Platform.runLater(() ->
+                this.debateThumbnailViewModel.listTag_selected_valueProperty().addListener(new WeakListChangeListener<>(change -> {
+                    while (change.next()) {
+                        if (change.wasAdded()) {
+                            change.getAddedSubList().forEach(item -> Platform.runLater(() -> flowTag.getChildren().add(item.getView())));
+                        } else if (change.wasRemoved()) {
+                            change.getRemoved().forEach(item -> flowTag.getChildren().remove(item.getView()));
+                        }
+                    }
+                })));
+        this.lblNbPeople.textProperty().bind(this.debateThumbnailViewModel.lblNbPeople_valueProperty());
+    }
+
+
+    @Override
+    public void onViewAdded_Cycle() {
+    }
+
+    @Override
+    public void onViewRemoved_Cycle() {
+    }
+}
---
+++ src/main/java/fr/univlr/debathon/application/viewmodel/mainwindow/debate/DebateThumbnailViewModel.java
@@ -0,0 +1,92 @@
+package fr.univlr.debathon.application.viewmodel.mainwindow.debate;
+
+import de.saxsys.mvvmfx.InjectScope;
+import de.saxsys.mvvmfx.ViewTuple;
+import fr.univlr.debathon.application.view.mainwindow.debate.items.TagView;
+import fr.univlr.debathon.application.viewmodel.ViewModel_SceneCycle;
+import fr.univlr.debathon.application.viewmodel.mainwindow.MainViewScope;
+import fr.univlr.debathon.application.viewmodel.mainwindow.debate.items.TagViewModel;
+import fr.univlr.debathon.log.generate.CustomLogger;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+
+public class DebateThumbnailViewModel extends ViewModel_SceneCycle {
+
+    private static final CustomLogger LOGGER = CustomLogger.create(DebateThumbnailViewModel.class.getName());
+
+    /*TODO Model
+    private final Debate;*/
+
+    //Value
+    private final StringProperty lblTitle_value = new SimpleStringProperty("/");
+    private final ListProperty<ViewTuple<TagView, TagViewModel>> listTag_selected_value = new SimpleListProperty<>(FXCollections.observableArrayList());
+    private final StringProperty lblNbPeople_value = new SimpleStringProperty("/");
+
+    @InjectScope
+    private MainViewScope mainViewScope;
+
+
+    /**
+     * Default constructor
+     *
+     * @author Gaetan Brenckle
+     */
+    public DebateThumbnailViewModel() {
+        if (LOGGER.isTraceEnabled()) {
+            LOGGER.trace("[public][constructor] Creation of the DebateThumbnailViewModel() object.");
+        }
+
+        //TODO link Model with data
+    }
+
+    public void initialize() {
+        System.out.println(String.format("Scope init %s", mainViewScope));
+    }
+
+
+    //Value
+    /**
+     * Property of the variable lblTitle_value.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @return {@link StringProperty} - return the property of the variable lblTitle_value.
+     */
+    public StringProperty lblTitle_valueProperty() {
+        return lblTitle_value;
+    }
+
+    /**
+     * Property of the variable listTag_selected_value.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @return {@link StringProperty} - return the property of the variable listTag_selected_value.
+     */
+    public ListProperty<ViewTuple<TagView, TagViewModel>> listTag_selected_valueProperty() {
+        return listTag_selected_value;
+    }
+
+    /**
+     * Property of the variable lblNbPeople_value.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @return {@link StringProperty} - return the property of the variable lblNbPeople_value.
+     */
+    public StringProperty lblNbPeople_valueProperty() {
+        return lblNbPeople_value;
+    }
+
+
+    @Override
+    public void onViewAdded_Cycle() {
+    }
+
+    @Override
+    public void onViewRemoved_Cycle() {
+    }
+}

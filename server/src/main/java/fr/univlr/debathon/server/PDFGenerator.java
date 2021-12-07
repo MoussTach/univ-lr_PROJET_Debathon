b9f5fab7e5c9c73c1511b14ac0e9324c071d900e
Gautier <gautier.pitek@etudiant.univ-lr.fr>
2021-12-07 15:24:07 +0100

    Update PDFGenerator

--- server/src/main/java/fr/univlr/debathon/server/PDFGenerator.java
+++ server/src/main/java/fr/univlr/debathon/server/PDFGenerator.java
@@ -1,52 +1,48 @@
 package fr.univlr.debathon.server;
 
+import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
+import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfWriter;
+import org.jfree.chart.JFreeChart;
 
 public class PDFGenerator {
-
+    public static Connection c;
     public PDFGenerator(){}
 
-    public Document getPDF(){
+    public Document getPDF(PDFquestion question){
         Document pdf = new Document(PageSize.A4);
-        try {
-            PdfWriter.getInstance(pdf,new FileOutputStream("test.pdf"));
-            pdf.open();
-            pdf.addTitle("Test Title");
-            pdf.add(new Paragraph("Hello World"));
+        JFreeChart chart = ChartGenerator.getInstance().genPieChart(question);
 
-        }
-        catch (DocumentException | FileNotFoundException e) {
+        try {
+            //
+            PdfWriter writer = PdfWriter.getInstance(pdf,new FileOutputStream("recapitulatif.pdf"));
+            pdf.open();
+            pdf.addTitle("Récapitulatif");
+            Paragraph p = new Paragraph("Récapitulatif");
+            p.setAlignment("Center");
+            pdf.add(p);
+            BufferedImage buffer = chart.createBufferedImage((int) (pdf.getPageSize().getWidth()/2),(int) (pdf.getPageSize().getHeight()/5));
+            Image image = Image.getInstance(writer,buffer,1.0f);
+            image.scaleToFit(PageSize.A4.getWidth()/2, image.getScaledWidth()/2);
+            float x = (PageSize.A4.getWidth() - image.getScaledWidth()) / 2;
+            image.setAbsolutePosition(x, image.getAbsoluteY());
+            pdf.add(image);
+
+
+        } catch (DocumentException | IOException e) {
             e.printStackTrace();
         }
+
         pdf.close();
         return pdf;
     }
 
-
-
-
-
-
-
-    public static Connection c;
-
-    public static void main(String[] args) {
-        try {
-            c = DriverManager.getConnection("jdbc:sqlite:server/db_debathon.db"); //Creation connection on db
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        PDFdata.getRequest1(1);
-    }
 
 }
